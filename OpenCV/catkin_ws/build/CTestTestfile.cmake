41320b281df7869d3109d2812f7eb7db5540cf22
KhalidOwlWalid <khalidowlwalid@gmail.com>
2021-12-24 16:18:07 +0000

    initial commit

---
+++ OpenCV/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shenowlshoot/catkin_ws/src
+# Build directory: /home/shenowlshoot/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("unit2")
